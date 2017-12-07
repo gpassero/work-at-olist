b69b595a6ee47c21e87460de8f12f0baf0a20060
Guilherme Passero <guilherme.passer@gmail.com>
2017-12-07 01:22:19 -0200

    Add models for channels and categories

--- work-at-olist/core/models.py
+++ work-at-olist/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Channel(models.Model):
+    """A model for channels used by sellers to publish their products.
+
+    Attributes:
+        name    The name of the channel
+    """
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        """Return the name of the channel."""
+        return self.name
+
+
+class Category(models.Model):
+    """A model for channels' categories of products.
+
+    Attributes:
+        name    The name of the category with its path (e.g. Games / XBOX 360 / Console)
+    """
+    name = models.CharField(max_length=200)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Return the name of the category."""
+        return self.name
