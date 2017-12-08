20c4e230e840d5570037543b4ee2943ab646e228
Guilherme Passero <guilherme.passer@gmail.com>
2017-12-08 12:48:41 -0200

    Update SECRET_KEY and DEBUG settings for production
    SECRET_KEY is now generated with a random sequence of chars.
    DEBUG is now loaed from "DEBUG" env variable.

--- work-at-olist/workatolist/settings.py
+++ work-at-olist/workatolist/settings.py
@@ -1,4 +1,8 @@
 import os
+import random
+import string
+
+from prettyconf import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -7,11 +11,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ld7s4bcrf)mso_*y^mnnfrr=fh&)4gs$*e%-acjxw5nyulpioz'
+SECRET_KEY = ''.join([random.SystemRandom().choice("{}{}{}".format(
+    string.ascii_letters, string.digits, string.punctuation)) for i in range(50)])
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=config.boolean)
 
 ALLOWED_HOSTS = []
 
