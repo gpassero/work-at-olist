c219bbbbd7d5afff50c61b1c4d1223f1574a3e27
Guilherme Passero <guilherme.passer@gmail.com>
2017-12-09 00:41:14 -0200

    Add uuid field to channel and category models and serializers

--- work-at-olist/core/models.py
+++ work-at-olist/core/models.py
@@ -7,25 +7,29 @@ class Channel(models.Model):
     """A model for channels used by sellers to publish their products.
 
     Attributes:
+        uuid        Unique identifier for the channel
         name        The name of the channel
         categories  The categories of the channel
     """
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
         """Return the name of the channel."""
-        return self.name
+        return 'Channel %s - %s' % (self.uuid, self.name)
 
 
 class Category(models.Model):
     """A model for channels' categories of products.
 
     Attributes:
+        uuid            Unique identifier for the category
         name            The name of the category
         parent          The parent category
         channel         The parent channel
         subcategories   The subcategories of the channel
     """
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
     parent = models.ForeignKey('self', null=True, on_delete=models.CASCADE, related_name='subcategories')
     channel = models.ForeignKey(Channel, on_delete=models.CASCADE, related_name='categories')
@@ -35,4 +39,4 @@ class Category(models.Model):
 
     def __str__(self):
         """Return the name of the category."""
-        return self.name
+        return 'Category %s - %s' % (self.uuid, self.name)
--- work-at-olist/core/serializers.py
+++ work-at-olist/core/serializers.py
@@ -3,26 +3,32 @@ from rest_framework import serializers
 from core.models import Channel, Category
 
 
-class ChannelSerializer(serializers.ModelSerializer):
-    """Serialize a channel and its categories."""
-    class Meta:
-        model = Channel
-        editable = False
-        fields = ('name', 'categories')
-
-
 class EntityNameSerializer(serializers.Serializer):
     """Serialize an entity's name."""
     name = serializers.CharField(max_length=200)
 
 
+class FKSerializer(serializers.Serializer):
+    """Serialize an entity's uuid and name."""
+    uuid = serializers.UUIDField()
+    name = serializers.CharField(max_length=200)
+
+
+class ChannelSerializer(serializers.ModelSerializer):
+    """Serialize a channel and its categories."""
+    class Meta:
+        model = Channel
+        editable = False
+        fields = ('uuid', 'name', 'categories')
+
+
 class CategorySerializer(serializers.ModelSerializer):
     """Serialize a category with its related categories and channel."""
-    channel = EntityNameSerializer()
-    parent = EntityNameSerializer()
-    subcategories = EntityNameSerializer(many=True)
+    channel = FKSerializer()
+    parent = FKSerializer()
+    subcategories = FKSerializer(many=True)
 
     class Meta:
         model = Category
         editable = False
-        fields = ('name', 'channel', 'parent', 'subcategories')
+        fields = ('uuid', 'name', 'channel', 'parent', 'subcategories')
