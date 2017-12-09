cd8f29b3f776fa9b53706161499b8002b9dc3dad
Guilherme Passero <guilherme.passer@gmail.com>
2017-12-09 01:08:30 -0200

    Update api views to use uuid instead of name as entity identifier
    The API must now be accessed at the following paths:
    /channels/ (list all channels)
    /channels/<channel-uuid> (retrieve a single channel and its categories)
    /categories/<category-uuid> (retrieve a single category and its parent and subcategories)

--- work-at-olist/api/urls.py
+++ work-at-olist/api/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import include, url
 from django.http import HttpResponse
 from rest_framework import routers
 
-from api.views import ChannelsView
+from api.views import ChannelsView, CategoriesView
 
 router = routers.DefaultRouter()
 router.register(r'channels', ChannelsView)
+router.register(r'categories', CategoriesView)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
--- work-at-olist/api/views.py
+++ work-at-olist/api/views.py
@@ -2,12 +2,12 @@ import re
 
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, APIException
 from rest_framework.response import Response
 from rest_framework.serializers import ListSerializer
 
 from core.models import Channel, Category
-from core.serializers import ChannelSerializer, CategorySerializer, EntityNameSerializer
+from core.serializers import ChannelSerializer, CategorySerializer, FKSerializer
 
 
 class ChannelsView(viewsets.ReadOnlyModelViewSet):
@@ -17,28 +17,32 @@ class ChannelsView(viewsets.ReadOnlyModelViewSet):
 
     def list(self, request):
         """Return a list of channels."""
-        serializer = EntityNameSerializer(self.queryset, many=True)
+        serializer = FKSerializer(self.queryset, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
-        """Return a list of channels's categories."""
+        """Return a single channel and its categories."""
         try:
-            channel = Channel.objects.get(name__iexact=pk)
+            channel = Channel.objects.get(uuid=pk)
         except:
             raise NotFound
-        queryset = Category.objects.filter(channel=channel)
-        serializer = EntityNameSerializer(queryset, many=True)
+        serializer = ChannelSerializer(channel)
         return Response(serializer.data)
 
-    @detail_route(methods=['get', 'post'], url_path='(?P<category>.+)')
-    def list_category(self, request, pk=None, category=None):
+
+class CategoriesView(viewsets.ReadOnlyModelViewSet):
+    """A view for listing categories."""
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def list(self, request):
+        raise APIException('Can\'t list all categories. Check /channels/<channel-uuid> to see a list of categories of a channel.')
+
+    def retrieve(self, request, pk=None):
         """Return a single category and its parents and subcategories."""
         try:
-            # Check the requested channel and category exists
-            channel = Channel.objects.get(name__iexact=pk)
-            queryset = Category.objects.filter(channel=channel, name__iexact=category)
+            category = Category.objects.get(uuid=pk)
         except:
             raise NotFound
-        serializer = CategorySerializer(queryset, many=True)
+        serializer = CategorySerializer(category)
         return Response(serializer.data)
-
