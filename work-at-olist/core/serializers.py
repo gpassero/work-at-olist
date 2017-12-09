6c53cdb8041a10fab30d470524e6241fc036447b
Guilherme Passero <guilherme.passer@gmail.com>
2017-12-09 01:22:22 -0200

    Update channel serializer to show subcategories' parents

--- work-at-olist/core/serializers.py
+++ work-at-olist/core/serializers.py
@@ -3,20 +3,25 @@ from rest_framework import serializers
 from core.models import Channel, Category
 
 
-class EntityNameSerializer(serializers.Serializer):
-    """Serialize an entity's name."""
-    name = serializers.CharField(max_length=200)
-
-
 class FKSerializer(serializers.Serializer):
     """Serialize an entity's uuid and name."""
     uuid = serializers.UUIDField()
     name = serializers.CharField(max_length=200)
 
 
+class ChannelCategorySerializer(serializers.ModelSerializer):
+    """Serialize a category with its related categories and channel."""
+    parent = serializers.UUIDField(source='parent.uuid', read_only=True)
+
+    class Meta:
+        model = Category
+        editable = False
+        fields = ('uuid', 'name', 'parent')
+
+
 class ChannelSerializer(serializers.ModelSerializer):
     """Serialize a channel and its categories."""
-    categories = FKSerializer(many=True)
+    categories = ChannelCategorySerializer(many=True)
 
     class Meta:
         model = Channel
