0d61f3e1264b7ce90716032c5c397263f5cc70f1
Guilherme Passero <guilherme.passer@gmail.com>
2017-12-09 00:40:50 -0200

    Update api views to return categories' parent using serializers

--- work-at-olist/api/views.py
+++ work-at-olist/api/views.py
@@ -1,4 +1,3 @@
-import json
 import re
 
 from rest_framework import viewsets
@@ -7,18 +6,19 @@ from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.serializers import ListSerializer
 
-from core.models import Category, Channel
+from core.models import Channel, Category
+from core.serializers import ChannelSerializer, CategorySerializer, EntityNameSerializer
 
 
 class ChannelsView(viewsets.ReadOnlyModelViewSet):
-    """A view for listing channels."""
+    """A view for listing channels and its categories."""
     queryset = Channel.objects.all()
-    serializer_class = None
+    serializer_class = ChannelSerializer
 
     def list(self, request):
         """Return a list of channels."""
-        q = self.get_queryset().values_list('name', flat=True)
-        return Response(list(q))
+        serializer = EntityNameSerializer(self.queryset, many=True)
+        return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
         """Return a list of channels's categories."""
@@ -27,31 +27,18 @@ class ChannelsView(viewsets.ReadOnlyModelViewSet):
         except:
             raise NotFound
         queryset = Category.objects.filter(channel=channel)
-        q = queryset.values_list('name', flat=True)
-        return Response(list(q))
+        serializer = EntityNameSerializer(queryset, many=True)
+        return Response(serializer.data)
 
     @detail_route(methods=['get', 'post'], url_path='(?P<category>.+)')
     def list_category(self, request, pk=None, category=None):
         """Return a single category and its parents and subcategories."""
         try:
             # Check the requested channel and category exists
-            category = re.sub(r'(?<! )/(?! )', ' / ', category)
             channel = Channel.objects.get(name__iexact=pk)
-            category = Category.objects.get(channel=channel, name__iexact=category)
+            queryset = Category.objects.filter(channel=channel, name__iexact=category)
         except:
             raise NotFound
-        # Find the parent categories name using regex
-        parent_name = category.name
-        parents_names = []
-        while '/' in parent_name:
-            parent_name = re.sub(r'(/[^/]+)$',  '', parent_name).strip()
-            if parent_name != category.name and parent_name not in parents_names:
-                parents_names.append(parent_name)
-        parents_names = list(reversed(parents_names))
-        # Load subcategories that contain the name of the request category
-        # (the requested category in also loaded in this list)
-        subcategories = Category.objects.filter(channel=channel, name__istartswith=category.name)
-        subcategories_names = subcategories.values_list('name', flat=True)
-        # Generate the list of categories to be returned
-        categories_list = parents_names + list(subcategories_names)
-        return Response(categories_list)
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
+
